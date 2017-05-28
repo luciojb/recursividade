7e6cb118cc5c16865150bbd2b7982896c276f616
lucio <lucio@lucio-Inspiron-3421>
2017-05-28 11:00:05 -0300

    Just Commit

---
+++ Recursividade/src/main/ExercicioExemplo.java
@@ -0,0 +1,16 @@
+package main;
+
+import exercicios.Funcoes;
+
+public class ExercicioExemplo {
+
+	public static void main(String[] args) {
+		int num=5, num2=3, exp=4;
+		String nome="Ana";
+		
+		System.out.println(Funcoes.Fatorial(num));
+		System.out.println(Funcoes.ReverteNome(nome));
+		System.out.println(Funcoes.Expoente(num2, exp));
+	}
+
+}
